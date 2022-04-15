82569a6ede127fbf9c9723ef4e9eae33d5db4824
Muhammet Sait Akgunes <msa.git99@gmail.com>
2022-04-15 06:41:41 +0300

    jupyter notebook files changed to py.

---
+++ updated without tufe24.py
@@ -0,0 +1,58 @@
+# PLEASE USE JUPYTER NOTEBOOK AND RUN STEP BY STEP.
+
+import pandas as pd
+import seaborn as sbn
+import numpy as np
+
+dataFrame = pd.read_excel("C:/Users/Muhammet Sait/Desktop/deneme.xlsx")
+
+from sklearn.model_selection import train_test_split
+satis_fiyat = dataFrame["usd_satis"].values
+degerler = dataFrame[["usd_faiz_6","usd_faiz_12","usd_faiz_12p","tufe_12","dis_borc"]].values
+
+fiyat_train, fiyat_test, deger_train, deger_test = train_test_split(satis_fiyat,degerler,test_size=0.2,random_state=15)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(degerler)
+
+deger_train = scaler.transform(deger_train)
+deger_test = scaler.transform(deger_test)
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
+
+model.add(Dense(11,activation="relu"))
+model.add(Dense(17,activation="relu"))
+model.add(Dense(17,activation="relu"))
+model.add(Dense(17,activation="relu"))
+model.add(Dense(11,activation="relu"))
+
+model.add(Dense(1))
+
+model.compile(optimizer="rmsprop",loss="mse")
+
+model.fit(deger_train, fiyat_train, epochs=300)
+
+loss = model.history.history["loss"]
+sbn.lineplot(x=range(len(loss)),y = loss)
+
+trainLoss = model.evaluate(deger_train,fiyat_train, verbose=0)
+testLoss = model.evaluate(deger_test,fiyat_test, verbose=0)
+
+testTahminleri = model.predict(deger_test)
+
+tahminDf = pd.DataFrame(fiyat_test,columns=["Gerçek Fiyat"])
+testTahminleri = pd.Series(testTahminleri.reshape(375,))
+tahminDf = pd.concat([tahminDf,testTahminleri],axis=1)
+tahminDf.columns=["Gerçek Fiyat","Tahmin Fiyat"]
+
+#tahminDf
+
+sbn.scatterplot(x = "Gerçek Fiyat", y = "Tahmin Fiyat", data = tahminDf)
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+#mean_absolute_error(tahminDf["Gerçek Fiyat"], tahminDf["Tahmin Fiyat"])
